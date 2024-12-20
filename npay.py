c857724dcecec0c2d98cca7f5e45a28ffe704004
@mitt_ydv <146659530+Mittyadav@users.noreply.github.com>
2024-12-20 22:51:37 +0530

    Update npay.py

--- npay.py
+++ npay.py
@@ -5,8 +5,7 @@ import uuid
 from curl_cffi import requests
 from loguru import logger
 from fake_useragent import UserAgent
-from termcolor import colored
-from pyfiglet import figlet_format
+from utils.banner import banner
 from colorama import Fore, Style, init
 from datetime import datetime
 
@@ -33,29 +32,19 @@ browser_id = None
 account_info = {}
 last_ping_time = {}
 
-
 def uuidv4():
     return str(uuid.uuid4())
 
+def show_banner():
+    print(Fore.MAGENTA + banner + Style.RESET_ALL)
 
-def print_header():
-    """Displays the custom banner and welcome message."""
-    ascii_art = figlet_format("NodepayBot", font="slant")
-    colored_art = colored(ascii_art, color="cyan")
-    border = "=" * 40
-
-    print(border)
-    print(colored_art)
-    print(colored("by dark life", color="cyan", attrs=["bold"]))
-    print("\nWelcome to NodepayBot - Automate your tasks effortlessly!")
-    print(border)
-
+def show_copyright():
+    print(Fore.MAGENTA + Style.BRIGHT + banner + Style.RESET_ALL)
 
 def valid_resp(resp):
     if not resp or "code" not in resp or resp["code"] < 0:
         raise ValueError("Invalid response")
     return resp
-
 
 def load_tokens_from_file(filename):
     try:
@@ -66,7 +55,6 @@ def load_tokens_from_file(filename):
         logger.error(f"Failed to load tokens: {e}")
         raise SystemExit("Exiting due to failure in loading tokens")
 
-
 def load_proxies(proxy_file):
     try:
         with open(proxy_file, 'r') as file:
@@ -75,7 +63,6 @@ def load_proxies(proxy_file):
     except Exception as e:
         logger.error(f"Failed to load proxies: {e}")
         raise SystemExit("Exiting due to failure in loading proxies")
-
 
 def dailyclaim(token):
     url = DOMAIN_API["DAILY_CLAIM"]
@@ -111,22 +98,17 @@ def dailyclaim(token):
         log_message(f"Error in dailyclaim: {e}", Fore.RED)
         return False
 
-
 def is_valid_proxy(proxy):
     return True
-
 
 def load_session_info(proxy):
     return {}
 
-
 def save_session_info(proxy, data):
     pass
 
-
 def save_status(proxy, status):
     pass
-
 
 def handle_logout(proxy):
     global status_connect, account_info
@@ -134,7 +116,6 @@ def handle_logout(proxy):
     account_info = {}
     save_status(proxy, None)
     log_message(f"Logged out and cleared session info for proxy {proxy}", Fore.RED)
-
 
 async def start_ping(proxy, token):
     try:
@@ -145,7 +126,6 @@ async def start_ping(proxy, token):
         log_message(f"Ping task for proxy {proxy} was cancelled", Fore.YELLOW)
     except Exception as e:
         log_message(f"Error in start_ping for proxy {proxy}: {e}", Fore.RED)
-
 
 async def ping(proxy, token):
     global last_ping_time, RETRIES, status_connect
@@ -168,7 +148,7 @@ async def ping(proxy, token):
 
         response = await call_api(DOMAIN_API["PING"], data, proxy, token)
         if response["code"] == 0:
-            log_message(f"Ping SUCCESSFUL for {proxy} - IP Score {response['data']['ip_score']}", Fore.GREEN)
+            log_message(f"Ping SUCCESSFUL for {proxy} - IP Score {response['data']['ip_score']}", Fore.MAGENTA)  # Changed color to pink
             RETRIES = 0
             status_connect = CONNECTION_STATES["CONNECTED"]
         else:
@@ -176,7 +156,6 @@ async def ping(proxy, token):
     except Exception as e:
         log_message(f"Ping failed via proxy {proxy}: {e}", Fore.RED)
         handle_ping_fail(proxy, None)
-
 
 async def call_api(url, data, proxy, token):
     headers = {
@@ -188,15 +167,13 @@ async def call_api(url, data, proxy, token):
     }
 
     try:
-        response = requests.post(url, json=data, headers=headers, impersonate="safari15_5", proxies={
-            "http": proxy, "https": proxy}, timeout=15)
+        response = requests.post(url, json=data, headers=headers, impersonate="safari15_5", proxies={ "http": proxy, "https": proxy}, timeout=15)
 
         response.raise_for_status()
         return valid_resp(response.json())
     except Exception as e:
         log_message(f"Error during API call to {url} via proxy {proxy}: {e}", Fore.RED)
         raise ValueError(f"Failed API call to {url}")
-
 
 async def render_profile_info(proxy, token):
     global browser_id, account_info
@@ -219,8 +196,17 @@ async def render_profile_info(proxy, token):
             await start_ping(proxy, token)
     except Exception as e:
         log_message(f"Error in render_profile_info for proxy {proxy}: {e}", Fore.RED)
-        return proxy
-
+        error_message = str(e)
+        if any(phrase in error_message for phrase in [
+            "sent 1011 (internal error) keepalive ping timeout; no close frame received",
+            "500 Internal Server Error"
+        ]):
+            log_message(f"Removing error proxy from the list: {proxy}", Fore.RED)
+            remove_proxy_from_list(proxy)
+            return None
+        else:
+            log_message(f"Connection error: {e}", Fore.RED)
+            return proxy
 
 async def main():
     all_proxies = load_proxies(PROXY_FILE)
@@ -266,14 +252,13 @@ async def main():
             await asyncio.sleep(3)
     await asyncio.sleep(10)
 
-
 def log_message(message, color):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(color + f"[{timestamp}] {message}" + Style.RESET_ALL)
 
-
 if __name__ == '__main__':
-    print_header()
+    show_copyright()
+    log_message("RUNNING WITH PROXIES", Fore.WHITE)
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
